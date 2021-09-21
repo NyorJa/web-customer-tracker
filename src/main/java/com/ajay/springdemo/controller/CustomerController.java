dd06aa3329e310c6f3954bdd5b3df05a23c33d79
ajay297 <dangilajay9@gmail.com>
2021-09-21 13:48:25 +0530

    add method for customer

--- src/main/java/com/ajay/springdemo/controller/CustomerController.java
+++ src/main/java/com/ajay/springdemo/controller/CustomerController.java
@@ -2,9 +2,13 @@ package com.ajay.springdemo.controller;
 
 import java.util.List;
 
+import com.ajay.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ajay.springdemo.dao.CustomerDAO;
@@ -16,18 +20,37 @@ public class CustomerController {
 
 	// need to inject the customer dao
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
 		
 		// get customers from the dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		List<Customer> theCustomers = customerService.getCustomers();
 				
 		// add the customers to the model
 		theModel.addAttribute("customers", theCustomers);
 		
 		return "list-customers";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel){
+
+		Customer theCustomer=new Customer();
+
+		theModel.addAttribute("customer",theCustomer);
+
+		return  "customer-form";
+	}
+
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer){
+
+		customerService.saveCustomer(theCustomer);
+
+		return "redirect:/customer/list";
+
 	}
 	
 }
