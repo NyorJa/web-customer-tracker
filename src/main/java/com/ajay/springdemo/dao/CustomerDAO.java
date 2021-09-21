f338fb5dbd85a0a37f9e3eaf358869cb763d249c
ajay297 <dangilajay9@gmail.com>
2021-09-21 11:02:39 +0530

    spring mvc and hibernate web app

---
+++ src/main/java/com/ajay/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,13 @@
+package com.ajay.springdemo.dao;
+
+import java.util.List;
+
+
+
+import com.ajay.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+	
+}
---
+++ src/main/java/com/ajay/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,48 @@
+package com.ajay.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ajay.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+
+	@Autowired
+	private SessionFactory sessionFactory;
+			
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		// get the current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+				
+		// create a query  ... sort by last name
+		Query<Customer> theQuery = 
+				currentSession.createQuery("from Customer order by lastName",
+											Customer.class);
+		
+		// execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+				
+		// return the results		
+		return customers;
+	}
+
+	
+
+}
+
+
+
+
+
+
